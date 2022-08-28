da903da4608e498ff8abf1046d85eb93dabe5e7a
yashika0607 <63790665+yashika0607@users.noreply.github.com>
2022-08-28 19:40:10 +0530

    Create Dsa.py
    Write a Python Program to Illustrate Different Set Operations.
    Here we have defined two set variables and we have performed different set operations: union, intersection, difference and symmetric difference.

---
+++ Dsa.py
@@ -0,0 +1,14 @@
+A = {0, 1, 2, 3, 4, 5, 6, 8};
+B = {2,4};
+  
+# union
+print("Union :", A | B)
+  
+# intersection
+print("Intersection :", A & B)
+  
+# difference
+print("Difference :", A - B)
+  
+# symmetric difference
+print("Symmetric difference :", A ^ B)
